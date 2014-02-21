3be2e316a2f8456a86c2b3d4a76dbb1f9d909cf2
Xavier Combelle <xavier.combelle@gmail.com>
2014-02-21 13:21:41 +0100

    uniform branches (structure of the tree different of the sgf reprensentation) + limit for all branches

--- sgftool.py
+++ sgftool.py
@@ -32,104 +32,122 @@ def tokenize(file):
             
             yield "special",last_char
             last_char = next(f)
+from collections import namedtuple
+class Node:
+    def __init__(self):
+        self.properties = []
+        self.childs = []
+    def __repr__(self):
+        return "Node(properties="+repr(self.properties)+",childs="+repr(self.childs)+")"
 def tree(tokens):
     def print(*args): pass
-    token = next(tokens)
-    
-    while True:
+    current_node = Node()
+    stack=[current_node]
+    try:    
+        token = next(tokens)
         
-        print ("processing1",token)
-        
-        if token == ("special", "("):
-            print ("found (")
-            v = "variation",list(tree(tokens))
-            yield v
-            token = next(tokens)
-        
-        elif token == ("special", ")"):
-            print ("found )")
-            return
-        
-        elif token == ("special",";"):
-            print ("yielding node")
-            token = next(tokens)
-            type,value = token
-            node = []
-            print("testing property_id",token,repr(type))
-            while type == "property_id":
-                v=value
+        while True:
+            
+            print ("processing1",token)
+            
+            if token == ("special", "("):
+                print ("found (")
                 
-                values = []
-                token = next( tokens)
+                current_node.childs.append(Node())
+                current_node = current_node.childs[-1]
+                stack.append(current_node)
+                token = next(tokens)
+                assert token == ("special",";")
+                token = next(tokens)
+                
+            elif token == ("special", ")"):
+                print ("found )")
+                stack.pop()
+                current_node = stack[-1]
+                token = next(tokens)
+            elif token == ("special",";"):
+                print ("yielding node")
+                current_node.childs.append(Node())
+                current_node = current_node.childs[-1]
+                
+                token = next(tokens)
+            else:
                 type,value = token
-                
-                print ("consumed",token)
-                while type == "property_value":
+          
+                print("testing property_id",token,repr(type))
+                while type == "property_id":
+                    v=value
                     
-                    values.append(value)
+                    values = []
                     token = next( tokens)
                     type,value = token
-                    print ("testing",token)
                     
-                node.append( ("property",(v,values)))
-                print("adding to node",node)
-                print("next token",token)
-                print ("yielding node",node)
-            yield "node",node
+                    print ("consumed",token)
+                    while type == "property_value":
+                        
+                        values.append(value)
+                        token = next( tokens)
+                        type,value = token
+                        print ("testing",token)
+                        
+                    current_node.properties.append( (v,values))
+                    print("next token",token)
+    except StopIteration:
+        return stack.pop()        
 whitelist_property = set(
 """GM FF CA RU SZ KM PW PB WR BR DT EV RO PC SO
 AB AW AE B W""".split())
 
 def filter_properties(tree):
-    for type,value in tree:
-        if type == "property":
-            if value[0] in whitelist_property:
-                yield type,value
-        else:
-            yield type,list(filter_properties(value))
-          
-def cut_to_first_variation(tree):
     
-    for type, value in tree:
-        if type == "variation":
-            for v in cut_to_first_variation(value):
-                yield v
-            break
-        elif type == "node":
-            yield type, list(cut_to_first_variation(value))
-        elif type == "property":
-            yield type,value
-        else: 
-            raise Exception("invalid type "+type)
-
+    tree.properties = [(name,values) for name,values in tree.properties    
+                       if name in whitelist_property]
+    for child in tree.childs:
+        filter_properties(child)
 def first_variation(tree):
-    for type,value in tree:
-        yield type,list(cut_to_first_variation(value))
+    del tree.childs[1:]
+    if tree.childs:
+        first_variation(tree.childs[0])
+    
+def limit(tree,n):
+    if n == 0:
+        tree.childs = []
+    for child in tree.childs:
+        limit(child,n-1)
 
 def filter(tree):
-    tree= list(first_variation(tree))
-    tree=list(filter_properties(tree))
-    return tree
+    first_variation(tree)
+    filter_properties(tree)
+    
     
     
 def to_sgf(tree):
-    for type,value in tree:
-        if type=="variation":
+    return "("+"".join(node_to_sgf(tree))+")"
+def node_to_sgf(tree):
+    
+        
+    for name,values in tree.properties:
+        yield name
+        for value in values:
+            yield "["
+            yield value.replace("\\","\\\\").replace("]","\\]")
+            yield "]"
+    
+        
+    
+    for v in tree.childs:
+        yield '\n'
+        if len(tree.childs) > 1:
             yield "("
-            for v in to_sgf(value):
-                yield v
+    
+        yield ";"
+        
+        for x in node_to_sgf(v):
+            yield x
+        if len(tree.childs) >1:
             yield ")"
-        elif type == "node":
-            yield "\n"
-            yield ";"
-            for v in to_sgf(value):
-                yield v
-        elif type == "property":
-            yield value[0]
-            for v in value[1]:
-                yield "["
-                yield v.replace("\\","\\\\").replace("]","\\]")
-                yield "]"
+        
+    
 import pprint      
 def test():
     f=open("test.sgf",encoding="utf-8")
@@ -142,28 +160,27 @@ def test():
 def reverse(tree,size):
     def r(v):
         return v[0]+string.ascii_letters[size-string.ascii_letters.index(v[1])-1]
-    for type,value in tree:
-        if type == "property":
-            name,v = value
-            if name in {"AB","AW","AE","B","W"}:
-                #print(v)
-                #print(size-string.ascii_letters.index(v[0][1]))
-                v[0]=r(v[0])
-                #print (v)
-            elif name in {"AR","LN"}:
-                v=[":".join(r(coord) for coord in compound.split(":"))
-                   for compound in v]
-            elif name in {"CR","DD","MA","SL","SQ","TR"}:
-                v=[r(coord) for coord in v]
-            elif name in {"LB"}:
-                def switch(v):
-                    point,text=v.split(":")
-                    return r(point)+":"+text
-                v=[ switch(coord) for coord in v]
-            yield type,(name,v)
-        else:
-            yield type,list(reverse(value,size))
-#sys.exit(0)
+    for i,(name,value) in enumerate(tree.properties):
+        
+        if name in {"AB","AW","AE","B","W"}:
+            value[0]=r(value[0])
+            
+        elif name in {"AR","LN"}:
+            value=[":".join(r(coord) for coord in compound.split(":"))
+               for compound in v]
+        
+        elif name in {"CR","DD","MA","SL","SQ","TR"}:
+            value=[r(coord) for coord in value]
+        
+        elif name in {"LB"}:
+            def switch(v):
+                point,text=v.split(":",1)
+                return r(point)+":"+text
+            value=[ switch(coord) for coord in value]
+        tree.properties[i] = name,value
+    for child in tree.childs:
+        reverse(child,size)
+
 
 if __name__ == "__main__":
     #test()
@@ -171,30 +188,26 @@ if __name__ == "__main__":
     parser.add_argument("--filter", action='store_true',
                         help="filter coments and variation")
     parser.add_argument("--limit", action='store', type=int, default=None,
-                        help="limit the principal variation work only with with filter")
+                        help="limit the variations to max depth")
     parser.add_argument("--reverse",action="store_true",
                         help="reverse the board upside down")
     parser.add_argument("file",action="store")
     args = parser.parse_args()
-    if args.limit is not None and not args.filter:
-        parser.help()
-        sys.exit(1)
     new_file = tree(tokenize(open(args.file,encoding="utf-8")))
+    
     if args.filter:
-        new_file=filter(new_file)
-        if args.limit is not None:
-            del new_file[0][1][args.limit+1:]
+        filter(new_file)
+    
+    if args.limit is not None:
+        limit(new_file,args.limit+1)
+    
     if args.reverse:
         size = 19
-        new_file = list(new_file)
-        #pprint.pprint(new_file[0][1][0][1])
-        for p,(name,value) in new_file[0][1][0][1]:
+        for name,value in new_file.childs[0].properties:
             if name == "SZ":
                 size=int(value[0])
-        new_file = reverse(new_file,size)
+        reverse(new_file,size)
         
-    new_file = list(new_file)
-    #pprint.pprint(new_file)
     print ("".join(to_sgf(new_file)),
            file=open("new-"+args.file,"w",encoding="utf-8"))
 
